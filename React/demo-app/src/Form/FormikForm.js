0765e465fced2735d2e92e16502b92dbfc49c76b
zainabasif-21 <zainab.asif@dubizzlelabs.com>
2024-04-27 17:41:30 +0500

    Fixed the issues of validation in dynamic form my using state and feeding the validation schema after state changes

--- React/demo-app/src/Form/FormikForm.js
+++ React/demo-app/src/Form/FormikForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import './FormikForm.css'
@@ -7,78 +7,92 @@ import './FormikForm.css'
 export default function FormikForm({ formConfig, onSubmit }) {
   let initialValues = {}
   let validationSchema = {}
+  const [initialValuesState, setInitialValuesState] = useState({});
+  const [validationSchemaState, setValidationSchemaState] = useState(null);
 
   useEffect(() => {
     formConfig.fields.forEach((val) => {
-      validationSchema[val.name] = val.validation.isRequired ? Yup.string().required(val.name + ' is required') : false
+
+      switch (val.name) {
+        case 'email':
+          validationSchema[val.name] = Yup.string().email().required(val.name + ' is required')
+          break;
+        default:
+          validationSchema[val.name] = Yup.string().required(val.name + ' is required')
+          break;
+      }
     })
     formConfig.fields.forEach((val) => {
       initialValues[val.name] = val.initialValue
     })
-
-    console.log(validationSchema)
+    console.log(validationSchema, initialValues)
+    setInitialValuesState(initialValues);
+    setValidationSchemaState(validationSchema);
   }, [formConfig])
 
-  console.log(validationSchema, initialValues)
 
+  const renderForm = () => {
+    return (
+      <div>
+        <Formik
+          initialValues={initialValuesState}
+          validationSchema={Yup.object().shape(validationSchemaState)}
+          onSubmit={onSubmit}
+        >
+          {(formikProps) => (
+            <Form>
+              {formConfig.fields.map((field) => (
+                <div className='inputDiv' key={field.name}>
+                  <label >{field.label}</label>
+                  {field.type === "select" ? (
+                    <Field as="select" name={field.name}>
+                      {field.label}
+                      {field.options.map((option) => (
+                        <option name={option.label} key={option.value} value={option.value}>
+                          {option.label}
+                        </option>
+                      ))}
+                    </Field>
+                  ) : field.type === "radio" ? (
+                    // Render radio buttons
+                    <div>
+                      {field.options.map((option) => (
+                        <div key={option.value}>
+                          <Field
+                            type="radio"
+                            id={option.value}
+                            name={field.name}
+                            value={option.value}
+                          />
+                          <label htmlFor={option.value}>{option.label}</label>
+                        </div>
+                      ))}
+                    </div>
+                  ) : (
+                    // Render other input types (e.g., text, number, email)
+                    <Field
+                      name={field.name}
+                      type={field.inputType}
+                      component={field.component}
+                    />
+                  )}
+                  <ErrorMessage name={field.name} component="div" />
+                </div>
+              ))}
+              <button type="submit" disabled={!formikProps.isValid} onClick={() => {
+                onSubmit(formikProps.values)
+                formikProps.setSubmitting(true);
+                formikProps.resetForm();
+              }}>
+                Submit
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>);
+  }
 
   return (
-    <div>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={Yup.object().shape(validationSchema)}
-        onSubmit={onSubmit}
-      >
-        {(formikProps) => (
-          <Form>
-            {formConfig.fields.map((field) => (
-              <div className='inputDiv' key={field.name}>
-                <label >{field.label}</label>
-                {field.type === "select" ? (
-                  <Field as="select" name={field.name}>
-                    {field.label}
-                    {field.options.map((option) => (
-                      <option name={option.label} key={option.value} value={option.value}>
-                        {option.label}
-                      </option>
-                    ))}
-                  </Field>
-                ) : field.type === "radio" ? (
-                  // Render radio buttons
-                  <div>
-                    {field.options.map((option) => (
-                      <div key={option.value}>
-                        <Field
-                          type="radio"
-                          id={option.value}
-                          name={field.name}
-                          value={option.value}
-                        />
-                        <label for={option.value}>{option.label}</label>
-                      </div>
-                    ))}
-                  </div>
-                ) : (
-                  // Render other input types (e.g., text, number, email)
-                  <Field
-                    name={field.name}
-                    type={field.inputType}
-                    component={field.component}
-                  />
-                )}
-                <ErrorMessage name={field.name} component="div" />
-              </div>
-            ))}
-            <button type="submit" onClick={() => {
-              onSubmit(formikProps.values)
-              formikProps.resetForm();
-              formikProps.setSubmitting(false);
-            }}>
-              Submit
-            </button>
-          </Form>
-        )}
-      </Formik>
-    </div>
+    renderForm()
   )
 }
